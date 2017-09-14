97f3ca39de3d45386a205b7c78e198057b04a67a
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2017-09-14 21:54:31 +0200

    Fix the documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,31 @@
-/// Dimension of the window to create.
+//! # luminance windowing
+//!
+//! This is the base, abstract crate for windowing common types and functions in luminance. The
+//! `luminance` crate gives you all the abstraction over OpenGL, but it doesn’t give you a way to
+//! create an OpenGL context. This is due to the fact that creating and managing OpenGL contexts is
+//! tightly related to the type of application. Typical PC applications might need something like
+//! **GLFW** or **glutin** whilst others will directly bind to **X11** or **Windows API**. Several
+//! crate – `luminance-*` – exist to solve that problem. They all provide a different implementation
+//! for a simple need: opening an OpenGL context, opening a window and manage events. In theory, you
+//! could even have a `luminance-gtk` or `luminance-qt` to embed `luminance` in surfaces in those
+//! libraries.
+//!
+//! # What’s included
+//!
+//! This crate gives several important types that all backends must use. Among them, you’ll find:
+//!
+//! - `WindowDim`: abstraction over the dimension of a window and its mode (windowed, fullscreen).
+//! - `WindowOpt`: an opaque type giving access to hints to customize the window integration.
+//!
+//! The `Device` trait must be implemented by a backend so that an application is completely
+//! agnostic of the managing of the backend. This trait defines several basic methods that will help
+//! you to:
+//!
+//! - Retrieve the dimension of the window / framebuffer.
+//! - Iterate over the system events captured by your application.
+//! - Draw and swap the buffer chain.
+
+/// Dimension metrics.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum WindowDim {
   /// Windowed mode.
@@ -36,11 +63,20 @@ impl WindowOpt {
   }
 }
 
+/// Windowing device.
+///
+/// This type holds anything related to windowing – window system, events, mostly. The interface
+/// is straight forward, so feel free to have a look around.
 pub trait Device: Sized {
   /// Type of events.
   type Event;
 
   /// Iterator over events.
+  ///
+  /// # Note
+  ///
+  /// This associated type will disappean on long-term because it’s likely to be replaced by either
+  /// [ATC](https://github.com/rust-lang/rust/issues/44265) or [conservative `impl Trait` expanded](https://github.com/rust-lang/rust/issues/42183).
   type EventIter: Iterator<Item = Self::Event>;
 
   /// Type of device errors.
@@ -65,7 +101,7 @@ pub trait Device: Sized {
 
   // FIXME: as soon as either ATC (https://github.com/rust-lang/rust/issues/44265) or conservative
   // impl trait in methods (https://github.com/rust-lang/rust/issues/42183) are a real thing,
-  // changethe interface
+  // change the interface
   /// Retrieve an iterator over any pulled events.
   fn events<'a>(&'a mut self) -> Box<Iterator<Item = Self::Event> + 'a>;
 
