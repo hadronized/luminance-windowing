23dd50ce5bb983f0200dd1da8dbabac0e235c2ea
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2017-09-30 22:59:22 +0200

    0.1.0.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 0.1.0
+
+> Saturday, 30th of September, 2017
+
+- Initial revision.
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,26 @@
 //! # luminance windowing
 //!
 //! This is the base, abstract crate for windowing common types and functions in luminance. The
-//! `luminance` crate gives you all the abstraction over OpenGL, but it doesn’t give you a way to
+//! `luminance` crate provides you with abstracting over OpenGL, but it doesn’t give you a way to
 //! create an OpenGL context. This is due to the fact that creating and managing OpenGL contexts is
-//! tightly related to the type of application. Typical PC applications might need something like
-//! **GLFW** or **glutin** whilst others will directly bind to **X11** or **Windows API**. Several
-//! crate – `luminance-*` – exist to solve that problem. They all provide a different implementation
-//! for a simple need: opening an OpenGL context, opening a window and manage events. In theory, you
-//! could even have a `luminance-gtk` or `luminance-qt` to embed `luminance` in surfaces in those
-//! libraries.
+//! tightly related to the type of application you target. Typical PC applications might need
+//! something like **GLFW** or **glutin** whilst others will directly bind to **X11** or **Windows
+//! API**. Several crates – `luminance-*` – exist to solve that problem. They all provide a
+//! different implementation for a simple need: opening an OpenGL context, opening a window and
+//! manage events. In theory, you could even have a `luminance-gtk` or `luminance-qt` to embed
+//! `luminance` in surfaces in those libraries.
 //!
 //! # What’s included
 //!
-//! This crate gives several important types that all backends must use. Among them, you’ll find:
+//! This crate exposes several important types that all backends must use. Among them, you’ll find:
 //!
-//! - `WindowDim`: abstraction over the dimension of a window and its mode (windowed, fullscreen).
-//! - `WindowOpt`: an opaque type giving access to hints to customize the window integration.
+//! - `WindowDim`: abstraction over the dimension of a window and its mode (windowed, fullscreen,
+//!   fullscreen restricted).
+//! - `WindowOpt`: an opaque type giving access to hints to customize the window integration, such
+//!   as whether the cursor should be hidden or not.
 //!
 //! The `Device` trait must be implemented by a backend so that an application is completely
-//! agnostic of the managing of the backend. This trait defines several basic methods that will help
-//! you to:
+//! agnostic of the backend. This trait defines several basic methods that will help you to:
 //!
 //! - Retrieve the dimension of the window / framebuffer.
 //! - Iterate over the system events captured by your application.
@@ -43,6 +44,9 @@ pub struct WindowOpt {
 }
 
 impl Default for WindowOpt {
+  /// Defaults:
+  ///
+  /// - `hide_cursor(true)`
   fn default() -> Self {
     WindowOpt {
       hide_cursor: false
@@ -71,14 +75,6 @@ pub trait Device: Sized {
   /// Type of events.
   type Event;
 
-  /// Iterator over events.
-  ///
-  /// # Note
-  ///
-  /// This associated type will disappean on long-term because it’s likely to be replaced by either
-  /// [ATC](https://github.com/rust-lang/rust/issues/44265) or [conservative `impl Trait` expanded](https://github.com/rust-lang/rust/issues/42183).
-  type EventIter: Iterator<Item = Self::Event>;
-
   /// Type of device errors.
   type Error;
 
@@ -90,11 +86,19 @@ pub trait Device: Sized {
   fn size(&self) -> [u32; 2];
 
   /// Width of the framebuffer attached to the window.
+  ///
+  /// # Defaults
+  ///
+  /// Defaults to `.size()[0]`.
   fn width(&self) -> u32 {
     self.size()[0]
   }
 
   /// Height of the framebuffer attached to the window.
+  ///
+  /// # Defaults
+  ///
+  /// Defaults to `.size()[1]`.
   fn height(&self) -> u32 {
     self.size()[1]
   }
